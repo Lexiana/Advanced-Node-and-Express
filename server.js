6b1194324bf47bf65696021efb7e3782d572e17b
Lexiana <schaller.lexi@gmail.com>
2024-12-27 14:50:19 +0000

    feat: registration of new users

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -47,6 +48,45 @@ myDB(async client => {
 
   app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render('profile', { username: req.user.username });
+  });
+
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.route('/register').post((req, res, next)=>{
+    myDataBase.findOne({username: req.body.username}, (err, user)=>{
+      if(err){
+        next(err);
+      } else if (user){
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+        (err, doc) =>{
+          if(err){
+            res.redirect('/');
+          } else {
+            next(null, doc.ops[0]);
+          }
+        } 
+      )
+    }
+    })
+  }, passport.authenticate('local', {failureRedirect: '/'}),
+  (req, res, next) => {
+    res.redirect('/profile');
+  }
+);
+
+  app.use((req, res, next)=>{
+    req.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   passport.use(new LocalStrategy((username, password, done) => {
