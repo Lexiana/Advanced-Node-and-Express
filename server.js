fd54e8a44df51b90af1861786c16f61af7625a4c
Lexiana <schaller.lexi@gmail.com>
2024-12-27 16:55:46 +0000

    feat: Authentication with Socket.IO

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const passport = require('passport');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
 
+
 const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -20,7 +26,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
 app.use(passport.initialize());
@@ -32,6 +40,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+io.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,
+  key: 'express.sid',
+  secret: process.env.SESSION_SECRET,
+  store: store,
+  success: onAuthorizeSuccess,
+  fail: onAuthorizeFail
+}));
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
@@ -56,6 +72,18 @@ myDB(async client => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
+function onAuthorizeSuccess(data, accept){
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+};
+
+function onAuthorizeFail(data, message, error, accept){
+  if(error) throw new Error(message);
+  console.log('failed connection to socket.io: ', message);
+  accept(null, false);
+}
 
 
 
